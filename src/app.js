87b1e5875bfe84500c1956543e568cc47c37be31
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2023-01-19 10:59:11 +0200

    implemented the valid password s counter

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
-export {};
+import fs from 'fs';
+import { validate } from './validate.js';
+
+export const getNumberOfValidPasswords = (relativeFilePath) => {
+  const AbsolutePathToFile = new URL(relativeFilePath, import.meta.url);
+  const fileContents = fs.readFileSync(AbsolutePathToFile, 'utf-8');
+
+  let numberOfValidPasswords = 0;
+
+  fileContents.split(/\r?\n/).forEach(line => {
+    const isValid = validate(line);
+
+    if (isValid) {
+      numberOfValidPasswords++;
+    }
+  });
+
+  return numberOfValidPasswords;
+};
+
+const NumberOfValidPasswords = getNumberOfValidPasswords('./passwords.txt');
+
+// eslint-disable-next-line no-console
+console.log(NumberOfValidPasswords);
